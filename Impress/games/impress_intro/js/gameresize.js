b0bc3b45aef48e6de5848c34d5d37844fdf74a57
zmes-tw <zmes@email.zmes.tc.edu.tw>
2020-12-29 08:39:55 +0800

    Impress

---
+++ Impress/games/impress_intro/js/gameresize.js
@@ -0,0 +1,17 @@
+(function(){
+	function resize_canvas(){
+		now_sc = $(window).width()/ $(window).height();
+		set_sc = 16 / 9;
+		if(now_sc <= set_sc){
+			scale=($(window).width()/1280);
+		}else{
+			scale=($(window).height()/720);
+		}
+		$("#wrapper").css("width",1280*scale);
+		$("#wrapper").css("height",720*scale);
+	}
+	$(window).resize(function() {
+		resize_canvas();
+	});
+	resize_canvas();
+})();
