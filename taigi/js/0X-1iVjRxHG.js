206efd190743a23dda0cbe81778296bddeb24d43
zmes-tw <zmes@email.zmes.tc.edu.tw>
2021-03-19 08:33:37 +0800

    本土語言教學
    本土語言教學

---
+++ taigi/js/0X-1iVjRxHG.js
@@ -0,0 +1,4 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["k+In9"]); }
+
+__d("IntlQtEventFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f){"use strict";a=b("getFalcoLogPolicy_DO_NOT_USE")("1848815");c=b("FalcoLoggerInternal").create("intl_qt_event",a);e.exports=c}),null);
+__d("IntlRedundantStops",[],(function(a,b,c,d,e,f){e.exports=Object.freeze({equivalencies:{".":["\u0964","\u104b","\u3002"],"\u2026":["\u0e2f","\u0eaf","\u1801"],"!":["\uff01"],"?":["\uff1f"]},redundancies:{"?":["?",".","!","\u2026"],"!":["!","?","."],".":[".","!"],"\u2026":["\u2026",".","!"]}})}),null);
